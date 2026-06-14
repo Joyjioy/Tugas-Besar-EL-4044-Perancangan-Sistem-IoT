187afe6486a733be284174d32985702427eefd84
GalliumMetal31 <108174350+GalliumMetal31@users.noreply.github.com>
2026-06-14 18:22:07 +0700

    Update weather_service.py

--- Smart-Watering/Server/weather_service.py
+++ Smart-Watering/Server/weather_service.py
@@ -1,11 +1,13 @@
 import requests
 
+# Import credentials dan settings
 from config import (
     WEATHER_API_KEY,
     CITY,
     COUNTRY
 )
 
+# Global dictionary
 weather_data = {
     "rain": False,
     "humidity": 0,
@@ -14,9 +16,12 @@ weather_data = {
 
 
 def update_weather():
-
+    """
+    Mengambil data cuaca terkini dari API OpenWeatherMap.
+    Jika berhasil, fungsi ini akan memperbarui dictionary 'weather_data' secara in-place.
+    """
     try:
-
+        # Endpoint URL dan set parameter konfigurasi
         url = (
             f"https://api.openweathermap.org/data/2.5/weather"
             f"?q={CITY},{COUNTRY}"
@@ -24,20 +29,26 @@ def update_weather():
             f"&units=metric"
         )
 
+        # HTTP get request ke server OpenWeather
         response = requests.get(url)
 
+        # Parsing format JSON ke dictionary python
         data = response.json()
 
+        # Ekstraksi data suhu humidity dan temperature
         humidity = data["main"]["humidity"]
         temperature = data["main"]["temp"]
 
+        # Deteksi hujan
         rain = "rain" in data
 
+        # Memperbarui nilai dictionary global
         weather_data["rain"] = rain
         weather_data["humidity"] = humidity
         weather_data["temperature"] = temperature
 
         print("Weather Updated:", weather_data)
 
+    # Error Handling
     except Exception as e:
         print("Weather API Error:", e)
